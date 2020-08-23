0dfa4b12f3280710907dceb16c1212d820d0be69
PsorTheDoctor <psor2.0@gmail.com>
2020-08-23 20:30:58 +0200

    app.py

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask
+
+from api import create_api
+from db import db_session
+from cors import enable_cors
+
+
+def app():
+    app = Flask(__name__)
+    create_api(app)
+    enable_cors(app)
+
+    @app.teardown_appcontext
+    def shutdown_session(exception=None):
+        db_session.remove()
+
+    return app
+
+# if __name__ == '__main__':
+#     app.run(debug=False)
